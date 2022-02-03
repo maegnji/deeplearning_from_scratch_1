c2f76f7518e3dba0aac417a678fffdf5a10f375b
maeng <dooyoung90@gmail.com>
2022-02-03 12:59:55 +0900

    go

--- ch04/test.py
+++ ch04/test.py
@@ -40,11 +40,53 @@ def numerical_gradient(f,x):
     return grad
 
 
-
+# f : 최적화하려는 함수
+# init_x : 초기값
+# lr : learning rate (학습률)
+# step_num : 경사법에 따른 반복 횟수
 def gradient_descent(f, init_x, lr=0.01, step_num=100):
     x = init_x
 
     for i in range(step_num):
+        # numerical_gradient로 함수의 기울기 설정
         grad = numerical_gradient(f,x)
         x -= lr * grad
     return x
+
+
+# 그래프 그리기 위해서 x_history 추가
+def gradient_descent2(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+    x_history = []
+
+    for i in range(step_num):
+        x_history.append(x.copy())
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x, np.array(x_history)
+
+
+init_x = np.array([-3.0, 4.0])
+print(init_x)
+test = numerical_gradient(function_2, init_x)
+test2 = gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100)
+
+print(init_x)
+print(test)
+print(test2)
+
+
+# 그래프
+init_x = np.array([-3.0, 4.0])
+x, x_history = gradient_descent2(function_2, init_x, lr=0.1, step_num=20)
+
+plt.plot([-5, 5], [0, 0], '--b')
+plt.plot([0, 0], [-5, 5], '--b')
+plt.plot(x_history[:, 0], x_history[:, 1], 'o')
+
+plt.xlim(-3.5, 3.5)
+plt.ylim(-4.5, 4.5)
+plt.xlabel("X0")
+plt.ylabel("X1")
+plt.show()
