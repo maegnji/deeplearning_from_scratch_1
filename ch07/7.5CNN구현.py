e1e983ecf41162d2a110908a217757ec15c1f101
maeng <dooyoung90@gmail.com>
2022-02-27 22:59:14 +0900

    go

--- ch07/7.5CNN구현.py
+++ ch07/7.5CNN구현.py
@@ -14,6 +14,7 @@ from common.trainer import Trainer
 전체 구현은 simple_convnet.py 참고
 """
 class SimpleCovNet:
+    # input_dim : input데이터의 차원 (채널수, 높이, 너)
     # filter_num : 필터 수 / filter_size : 필터 크기 / stride : 스트라이드 / pad : 패딩
     # hidden_size : 은닉층 뉴런수 / output_size : 출력층 뉴런수 / weight_init_std : 초기화 때의 가중치 표준편차
     # 합성곱 계층의 parameter는 dictionary형태로 저장 (con_param)
@@ -32,8 +33,11 @@ class SimpleCovNet:
 
         # 가중치 매개변수 초기화
         self.params = {}
+        # 첫번째 매개변수 (필터수30, 채널1, 필터크기28, 필터크기28)
         self.params['W1'] = weight_init_std * np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
+        # 필터수 만큼 편향
         self.params['b1'] = np.zeros(filter_num)
+        
         self.params['W2'] = weight_init_std * np.random.randn(pool_output_size, hidden_size)
         self.params['b2'] = np.zeros(hidden_size)
         self.params['W3'] = weight_init_std * np.random.randn(hidden_size, output_size)
