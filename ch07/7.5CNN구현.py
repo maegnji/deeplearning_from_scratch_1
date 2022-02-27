7480b63efaabb4de696685dc7429df038d8f83c0
maeng <dooyoung90@gmail.com>
2022-02-28 07:08:46 +0900

    go

--- ch07/7.5CNN구현.py
+++ ch07/7.5CNN구현.py
@@ -29,6 +29,7 @@ class SimpleCovNet:
         input_size = input_dim[1]
         # 합성곱계층 출력크기 계산
         conv_output_size = (input_size - filter_size + 2*filter_pad) / filter_stride + 1
+        # 이거는 뭐지..
         pool_output_size = int(filter_num * (conv_output_size/2) * (conv_output_size/2))
 
         # 가중치 매개변수 초기화
@@ -37,8 +38,32 @@ class SimpleCovNet:
         self.params['W1'] = weight_init_std * np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
         # 필터수 만큼 편향
         self.params['b1'] = np.zeros(filter_num)
-        
+        # 두번째 매개변수
         self.params['W2'] = weight_init_std * np.random.randn(pool_output_size, hidden_size)
         self.params['b2'] = np.zeros(hidden_size)
+        # 세번째 매개변수
         self.params['W3'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b3'] = np.zeros(output_size)
+
+        # CNN을 구성하는 계층 생성 (딕셔너리 형태로 저장)
+        self.layers = OrderedDict()
+        self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'], conv_param['stride'], conv_param['pad'])
+        self.layers['Relu1'] = Relu()
+        self.layers['pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
+        self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
+        self.layers['Relu2'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W3'], self.params['b3'])
+        # 마지막 softmaxwithloss는 별도변수에 저장
+        self.last_layer = SoftmaxWithLoss()
+
+    # 추론 진행
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
+
+    # 손실 함수 (x : 입력데이터, t : 정답테이블)
+    def loss(self, x, t):
+        y = self.predict(x)
+        # 마지막층까지 계속 forward로 진행
+        return self.last_layer.forward(y, t)
